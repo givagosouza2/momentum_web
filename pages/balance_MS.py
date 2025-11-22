6c1b5d0f82124ec77d10f90fc7005e8e04a37023
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-11-22 17:37:26 -0300

    Update balance_MS.py

--- pages/balance_MS.py
+++ pages/balance_MS.py
@@ -195,7 +195,7 @@ def app():
 
             maxX = np.max(ellipse_fit[:, 0])
             maxY = np.max(ellipse_fit[:, 1])
-            %maxValue = np.max([maxX, maxY])
+            #maxValue = np.max([maxX, maxY])
             maxValue = 1
             if maxValue <= 0.1:
                 lim = 0.1
@@ -293,4 +293,5 @@ def app():
                         str(round(MF_energy_ML, 2)))
                 st.text('Energia das frequências altas ML (g^2) = ' +
                         str(round(HF_energy_ML, 2)))
+
 
